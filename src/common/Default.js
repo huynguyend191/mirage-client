02e8542c8aafc7663923702a0bd0716d5e5d8f53
huynguyend191 <huynguyend191@gmail.com>
2020-03-10 15:32:12 +0700

    Revamp default page

--- src/common/Default.js
+++ src/common/Default.js
@@ -1,37 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Default.module.css';
 import { Button, Layout, Divider } from 'antd';
 import FooterContent from './FooterContent';
-import introImg from '../resources/assets/flame-welcome.png';
+import introImg from '../assets/flame-welcome.png';
+import headerIcon from '../assets/header-icon.png';
+import RegisterModal from './RegisterModal';
 const { Header, Content, Footer } = Layout;
 
 export default function Default() {
+  const [isRegister, setRegister] = useState(false);
+  const [role, setRole] = useState("Student");
+  const showRegister = (role) => {
+    setRole(role);
+    setRegister(true);
+  }
+  const hideRegister = () => {
+    setRegister(false);
+  }
   return (
     <Layout>
+      <RegisterModal isVisible={isRegister} onClose={hideRegister} userRole={role} />
       <Header>
         <div className={styles.headerContent}>
-          <h1 className={styles.headerTitle}>MIRAGE</h1>
+          <div className={styles.headerWrapper}>
+            <img className={styles.headerIcon} src={headerIcon} alt="" />
+            <h1 className={styles.headerTitle}>MIRAGE</h1>
+          </div>
           <div className={styles.userButtonContainer}>
             <Button className={styles.userButton} type="secondary" shape="round">Login</Button>
-            <Button className={styles.userButton} type="primary" shape="round">Register</Button>
+            <Button className={styles.userButton} type="primary" shape="round" onClick={() => showRegister(role)}>Register</Button>
           </div>
         </div>
       </Header>
       <Content>
-      <div className={styles.defaultBody}>
-        <div className={styles.introContainer}>
-          <img className={styles.introImg} src={introImg} alt="" />
-          <div className={styles.introContent}>
-            <p className={styles.welcomeText}>Welcome to Mirage!</p>
-            <p className={styles.welcomeDescription}>Learn English anywhere you want</p>
-            <Button className={styles.registerButton} type="primary" shape="round">STUDY NOW</Button>
-            <Divider style={{width: "300px"}}>OR</Divider>
-            {/* <p className={styles.welcomeDescription}>Share your knowledge with others</p> */}
-            <Button className={styles.registerButton} type="secondary" shape="round">BECOME TUTOR</Button>
-            {/* <p className={styles.becomeTutor}>BECOME TUTOR</p> */}
+        <div className={styles.defaultBody}>
+          <div className={styles.introContainer}>
+            <img className={styles.introImg} src={introImg} alt="" />
+            <div className={styles.introContent}>
+              <p className={styles.welcomeText}>Welcome to Mirage!</p>
+              <p className={styles.welcomeDescription}>Learn English anywhere you want</p>
+              <Button className={styles.registerButton} type="primary" shape="round" onClick={() => showRegister("Student")}>STUDY NOW</Button>
+              <Divider style={{ width: "300px" }}>OR</Divider>
+              <Button className={styles.registerButton} type="secondary" shape="round" onClick={() => showRegister("Tutor")}>BECOME TUTOR</Button>
+            </div>
           </div>
         </div>
-      </div>
       </Content>
       <Footer>
         <FooterContent />
---
+++ src/common/RegisterModal.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Modal, Form, Input, Button, Checkbox } from 'antd';
+
+export default function RegisterModal({ userRole, isVisible, onClose }) {
+  return (
+    <Modal 
+      visible={isVisible} 
+      footer={null} 
+      title={userRole}
+      onCancel={onClose}
+    >
+      <Form
+        name="basic"
+        initialValues={{ remember: true }}
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item name="remember" valuePropName="checked">
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item >
+          <Button type="primary" htmlType="submit">
+            Submit
+        </Button>
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+}
