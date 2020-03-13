bc5d6652cd538e2135e6c81461dfe60e3012f916
huynguyend191 <huynguyend191@gmail.com>
2020-03-13 14:06:40 +0700

    change host url

--- src/lib/utils/axiosConfig.js
+++ src/lib/utils/axiosConfig.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'development'
-? 'https://localhost:3003'
-: 'http://example.com' ;
+const baseURL = "https://mirage-video-call.herokuapp.com/api"
 
 const axiosRequest = axios.create({
   baseURL,
