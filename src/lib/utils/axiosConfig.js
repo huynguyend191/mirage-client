d6fdd8368978224574e1b348131963adc9f5abe4
huynguyend191 <huynguyend191@gmail.com>
2020-03-13 14:35:56 +0700

    update server URL

--- src/lib/utils/axiosConfig.js
+++ src/lib/utils/axiosConfig.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const baseURL = "https://mirage-video-call.herokuapp.com/api"
+const baseURL = process.env.NODE_ENV === 'development'
+? 'https://localhost:3003/api'
+: 'https://mirage-video-call.herokuapp.com/api' ;
 
 const axiosRequest = axios.create({
   baseURL,
