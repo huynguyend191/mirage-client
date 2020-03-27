2ffe25823b8038436b7526302241414f0a66e5c1
huynguyend191 <huynguyend191@gmail.com>
2020-03-27 18:08:54 +0700

    add socket client

---
+++ src/lib/utils/socket.js
@@ -0,0 +1,13 @@
+import io from 'socket.io-client';
+import Cookies from 'universal-cookie';
+const cookies = new Cookies();
+
+const accessToken = cookies.get('access_token');
+
+const socketURL = process.env.NODE_ENV === 'development'
+? 'https://localhost:3003'
+: 'https://mirage-video-call.herokuapp.com' ;
+
+const socket = io(`${socketURL}?token=${accessToken}`);
+
+export default socket;
